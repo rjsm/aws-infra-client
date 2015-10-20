7405c9d5aa1f7e09b16da5cd6e0c4dde3d421afd
Ross Smith <rjsm@umich.edu>
2015-10-20 15:45:04 -0400

    move to ansible for management

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -8,48 +8,14 @@
 #
 #setup salt minion 
 #
-yum_package "salt-minion" do
+yum_package "ansible" do
   action :upgrade
-end
-
-service 'salt-minion' do
-  action [:enable, :start] 
 end
 
 #drop config file
-template "/etc/salt/minion" do
-  source 'minion.erb'
+template "/etc/cron.d/ansible-pull" do
+  source 'pull.erb'
   owner "root"
   group "root"
   mode "0644"
-  notifies :restart, 'service[salt-minion]', :delayed
-  # notifies :run, 'execute[wait for salt-minion]', :delayed
 end
-
-
-#
-#setup syslog monitoring
-#
-file "/etc/rsyslog.conf" do
-  owner 'root'
-  group 'root'
-  mode  '600'
-  action :create
-end
-
-#
-# setup yum-cron-security
-#
-yum_package "yum-cron-security" do
-  action :upgrade
-end
-
-file "/etc/yum/yum-cron-security.conf" do
-  owner 'root'
-  group 'root'
-  mode  '644'
-end
-#
-#setup ganglia/monitoring 
-#
-
